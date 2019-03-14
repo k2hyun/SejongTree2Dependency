adc3445ee36cd61157f0c5f0bb2a398683f726ab
Jamie <jamie.lim@kakaocorp.com>
2019-03-14 10:57:52 +0900

    토큰 길이가 1 < len <= 50으로 필터링

--- split.py
+++ split.py
@@ -30,15 +30,18 @@ def load_data(filename, valid_rate=10, test_rate=10):
                 fields[2] = fields[2].replace("++", "+PLS").replace("+", " ").replace("PLS", "+")
                 tokens.append("\t".join(fields))
             else:
-                sentences.append(tokens)
+                if 1 < len(tokens) <= 50:
+                    sentences.append(tokens)
                 tokens = []
 
     total_count = len(sentences)
 
     random.shuffle(sentences)
 
-    test_count = int(len(sentences)*(test_rate%100)/100) if test_rate else 0
-    valid_count = int(len(sentences)*(valid_rate%100)/100) if valid_rate else 0
+    # test_count = int(len(sentences)*(test_rate%100)/100) if test_rate else 0
+    # valid_count = int(len(sentences)*(valid_rate%100)/100) if valid_rate else 0
+    test_count = 1000
+    valid_count = 1000
     train_count = len(sentences) - valid_count - test_count
     valid_data = sentences[-valid_count:]
     sentences = sentences[0:-valid_count]
@@ -59,11 +62,12 @@ def save_data(filename, data):
         for sent in data:
             print("\n".join(sent), file=ofp)
             print("", file=ofp)
-            
+
 
 train, valid, test = load_data(sys.argv[1])
 prefix = sys.argv[2] if len(sys.argv) > 2 else sys.argv[1].split(".")[-2]
 
+os.makedirs("temp", exist_ok=True)
 save_data(f"temp/{prefix}.train.txt", train)
 save_data(f"temp/{prefix}.valid.txt", valid)
 save_data(f"temp/{prefix}.test.txt", test)
